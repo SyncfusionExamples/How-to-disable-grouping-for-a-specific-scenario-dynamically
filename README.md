826e5940a80e5df0726adc7a446eba609876bcb9
sudarsanMS <sudarsan.m@syncfusion.com>
2025-05-30 10:10:14 +0530

    SB for grouping dynamic changes

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# How-to-disable-grouping-for-a-specific-scenario-dynamically
-This demo shows how to disable grouping for a specific scenario dynamically in .NET MAUI DataGrid (SfDataGrid)
+The [.NET MAUI DataGrid](https://www.syncfusion.com/maui-controls/maui-datagrid) allows you to disable grouping dynamically by clearing the `GroupColumnDescriptions` collection. Additionally, if you want to remove grouping when there's only one group, you can use the following code snippet. The code below illustrates how to dynamically disable grouping for a specific scenario.
+
+
+ ##### C#
+ ```c#
+public MainPage()
+{
+	InitializeComponent();
+	viewModel.InstalledModels.CollectionChanged += InstalledModels_CollectionChanged;
+}
+
+private void InstalledModels_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+{
+	if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+	{
+		if (datagrid.View!.Groups.Count() == 1)
+		{
+			datagrid.GroupColumnDescriptions.Clear();
+		}
+	}
+}
+ ```
+ 
+
+
+Download the complete sample from [GitHub](https://github.com/SyncfusionExamples/How-to-disable-grouping-for-a-specific-scenario-dynamically/tree/Dynamic_Change_Grouping)
+
+##### Conclusion
+
+I hope you enjoyed learning how to disable grouping for a specific scenario dynamically. You can refer to our [.NET MAUI DataGrid feature tour](https://www.syncfusion.com/maui-controls/maui-datagrid) page to learn about its other groundbreaking features. Explore our [.NET MAUI DataGrid Documentation](https://help.syncfusion.com/maui/datagrid/getting-started) to understand how to present and manipulate data. For current customers, check out our .NET MAUI components on the [License and Downloads](https://www.syncfusion.com/account/downloads) page. If you are new to Syncfusion®, try our 30-day [free trial](https://www.syncfusion.com/downloads/maui) to explore our .NET MAUI DataGrid and other .NET MAUI components. Please let us know in the comments section if you have any queries or require clarification. You can also contact us through our [support forums](https://www.syncfusion.com/forums), [Direct-Trac](https://support.syncfusion.com/create) or [feedback portal](https://www.syncfusion.com/feedback/maui?control=sfdatagrid), or feedback portal. We are always happy to assist you!
---
+++ SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,133 @@
+﻿using SfDataGridSample.Helper;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            viewModel.InstalledModels.CollectionChanged += InstalledModels_CollectionChanged;
+        }
+
+        private void InstalledModels_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+            {
+                if (datagrid.View?.Groups?.Count() == 1)
+                {
+                    datagrid.GroupColumnDescriptions.Clear();
+                }
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            viewModel.InstalledModels.Add(new InstalledModel()
+            {
+                Architecture = "x64",
+                ParameterSize = 12345,
+                Publisher = "DEF Corp",
+                FullModel = "Model B",
+                Quantization = "Int16",
+                HumanReadableSize = "13.5 MB",
+                ModifiedAt = DateTime.Now,
+                Location = "Data Center A"
+            });
+
+        }
+    }
+
+    public class InstalledModel
+    {
+        public string Architecture { get; set; }
+        public int ParameterSize { get; set; }
+        public string Publisher { get; set; }
+        public string FullModel { get; set; }
+        public string Quantization { get; set; }
+        public string HumanReadableSize { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        public string Location { get; set; } // Grouping column
+    }
+
+    public class InstalledModelsViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<InstalledModel> _installedModels;
+
+        public ObservableCollection<InstalledModel> InstalledModels
+        {
+            get => _installedModels;
+            set
+            {
+                if (_installedModels != value)
+                {
+                    _installedModels = value;
+                    OnPropertyChanged(nameof(InstalledModels));
+                }
+            }
+        }
+
+        public ICommand AddMoreCommand { get; }
+
+        public InstalledModelsViewModel()
+        {
+            InstalledModels = new ObservableCollection<InstalledModel>();
+
+
+            var model = new InstalledModel()
+            {
+                Architecture = "x64",
+                ParameterSize = 12345,
+                Publisher = "ABC Corp",
+                FullModel = "Model A",
+                Quantization = "Int8",
+                HumanReadableSize = "12.3 MB",
+                ModifiedAt = DateTime.Now,
+                Location = "Data Center A"
+            };
+            InstalledModels.Add(model);
+            var model1 = new InstalledModel
+            {
+                Architecture = "ARM",
+                ParameterSize = 6789,
+                Publisher = "XYZ Inc",
+                FullModel = "Model B",
+                Quantization = "FP16",
+                HumanReadableSize = "8.9 MB",
+                ModifiedAt = DateTime.Now.AddDays(-1),
+                Location = "Data Center A"
+            };
+            InstalledModels.Add(model1);
+
+
+        }
+
+        private void AddMore()
+        {
+            InstalledModels.Add(new InstalledModel
+            {
+                Architecture = "RISC-V",
+                ParameterSize = 4567,
+                Publisher = "RISC Publisher",
+                FullModel = "Model C",
+                Quantization = "FP32",
+                HumanReadableSize = "14.7 MB",
+                ModifiedAt = DateTime.Now.AddDays(-2),
+                Location = "Data Center C"
+            });
+
+            // Add more items or different logic here if needed
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
